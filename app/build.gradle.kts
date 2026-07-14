f284dcd7c8c586d48501285955b97d08b728b634
elfennani <elfennani2002@gmail.com>
2026-07-14 23:45:26 +0100

    First Init

---
+++ app/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "com.elfen.clipkeep"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.elfen.clipkeep"
+        minSdk = 29
+        targetSdk = 36
+        compileSdk = 37
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.junit)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+
+    implementation("androidx.media3:media3-transformer:1.10.1")
+    implementation("androidx.media3:media3-effect:1.10.1")
+    implementation("androidx.media3:media3-common:1.10.1")
+}
---
+++ app/src/main/java/com/elfen/clipkeep/MainActivity.kt
@@ -0,0 +1,154 @@
+package com.elfen.clipkeep
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.provider.OpenableColumns
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.OptIn
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.InputTransformation
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.net.toFile
+import androidx.media3.common.MediaItem
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.transformer.ProgressHolder
+import androidx.media3.transformer.Transformer
+import com.elfen.clipkeep.ui.theme.ClipKeepTheme
+import java.io.File
+
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+class MainActivity : ComponentActivity() {
+    @OptIn(UnstableApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ClipKeepTheme {
+                var video by remember { mutableStateOf<Uri?>(null) }
+                val launcher =
+                    rememberLauncherForActivityResult(ActivityResultContracts.OpenDocument()) {
+                        video = it
+                    }
+                val startSeconds = rememberTextFieldState("0");
+                val endSeconds = rememberTextFieldState("1")
+
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        verticalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        Button(
+                            onClick = {
+                                launcher.launch(arrayOf("video/*"))
+                            }
+                        ) {
+                            Text("Pick Video")
+                        }
+
+                        if (video != null) {
+                            Text("URI: $video", modifier = Modifier.fillMaxWidth())
+
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.spacedBy(8.dp)
+                            ) {
+                                TextField(
+                                    modifier = Modifier.weight(1f),
+                                    state = startSeconds,
+                                    label = {
+                                        Text("Start Seconds")
+                                    },
+                                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                                )
+                                TextField(
+                                    modifier = Modifier.weight(1f),
+                                    state = endSeconds,
+                                    label = {
+                                        Text("End Seconds")
+                                    },
+                                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                                )
+                            }
+
+                            Button(
+                                onClick = {
+                                    Log.d("MainActivity", "Starting");
+                                    val inputMediaItem =
+                                        MediaItem.Builder()
+                                            .setUri(video)
+                                            .setClippingConfiguration(
+                                                MediaItem.ClippingConfiguration.Builder()
+                                                    .setStartPositionMs(
+                                                        startSeconds.text.toString().toInt() * 1000L
+                                                    )
+                                                    .setEndPositionMs(
+                                                        endSeconds.text.toString().toInt() * 1000L
+                                                    )
+                                                    .build()
+                                            )
+                                            .build()
+                                    val transformer =
+                                        Transformer.Builder(this@MainActivity)
+                                            .build();
+                                    val outputFile =
+                                        File(filesDir, this@MainActivity.getFileName(video!!))
+
+                                    transformer.start(inputMediaItem, outputFile.absolutePath)
+                                    Log.d("MainActivity", "Finished");
+                                }
+                            ) {
+                                Text("Crop Video")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun Context.getFileName(uri: Uri): String? {
+    contentResolver.query(
+        uri,
+        arrayOf(OpenableColumns.DISPLAY_NAME),
+        null,
+        null,
+        null
+    )?.use { cursor ->
+        val nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+        if (nameIndex != -1 && cursor.moveToFirst()) {
+            return cursor.getString(nameIndex)
+        }
+    }
+
+    return null
+}
